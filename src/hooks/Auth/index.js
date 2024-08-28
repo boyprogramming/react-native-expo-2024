46ff346bddf05fde1cbe99dc20999108af1b19d0
NG+ <155779046+boyprogramming@users.noreply.github.com>
2024-08-28 14:51:19 +0000

    [feat] Auth Context

--- src/hooks/Auth/index.js
+++ src/hooks/Auth/index.js
@@ -2,6 +2,13 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const AuthContext = createContext({});
 
+export const ROLE = {
+    SUPER: "SUPER",
+    ADM: "ADM",
+    USER: "USER"
+};
+
+
 export function AuthProvider({ children }) {
     const [user, setUser] = useState({
         authenticaded: null,
@@ -11,12 +18,32 @@ export function AuthProvider({ children }) {
 
     const signIn = async ({email, password}) => {
         if (email === "super@email.com" && password === "Super123") {
-             setUser({ id: 1, name: "super", email, role: "SUPER" });
-        }
-    
-        if (email === "adm@email.com" && password === "Adm123") {
-            setUser({ id: 1, name: "super", email, role: "ADM" });
-       }
+             setUser({ 
+                authenticaded: true,
+                user: {id: 1, name: "Super Usuário", email, role: "SUPER"},
+                role: ROLE.SUPER
+            });
+        } else if (email === "adm@email.com" && password === "Adm123") {
+            setUser({
+            authenticaded: true,
+            user: {id: 2, name: "Administrador Usuário", email, role: "ADM"},
+            role: ROLE.ADM
+        });
+       } else if (email === "user@email.com" && password === "User123") {
+        setUser({
+            authenticaded: true,
+            user: {id: 3, name: "Usuário", email, role: "USER"},
+            role: ROLE.USER
+
+        });
+   } else {
+        setUser({
+            authenticaded: false,
+            user: null,
+            role: null
+        });
+   }
+
    
     };
 
